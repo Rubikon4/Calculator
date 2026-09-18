be2f74a1cf7ed7b17dad17c571e5f0c49421aa10
Rubikon4 <Rubikon4@users.noreply.github.com>
2026-09-18 14:35:18 +0300

    Implement basic calculator input and operations

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,19 @@
 public class Calculator {
-    double operand;
-    char operator;
-    double result;
+    private double operand1;
+    private double operand2;
+    private char operator;
+    public double result;
 
-    public void getOperand(double operand) {
-        this.operand = operand;
+    public void getOperand(double operand, boolean isFirstOperand) {
+        if (isFirstOperand) {
+            this.operand1 = operand;
+        } else {
+            if (operand == 0 & this.operator == '/') {
+                System.out.println("На ноль делить нельзя!");
+            } else {
+                this.operand2 = operand;
+            }
+        }
     }
     public void getOperator(String operator) {
         int length = operator.length();
@@ -14,5 +23,12 @@ public class Calculator {
             System.out.println("Вы ввели неверный формат оператора. Подходят только '+', '-', '*', '/'");
         }
     }
-
+    public void calculate() {
+        switch (this.operator) {
+            case '+': result = operand1 + operand2; break;
+            case '-': result = operand1 - operand2; break;
+            case '*': result = operand1 * operand2; break;
+            case '/': result = operand1 / operand2; break;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Введите первое число: ");
+        calculator.getOperand(input.nextDouble(), true);
+        System.out.println("Введите оператор в простом виде (+ - * /): ");
+        calculator.getOperator(input.next());
+        System.out.println("Введите второе число: ");
+        calculator.getOperand(input.nextDouble(), false);
+        calculator.calculate();
+        System.out.println("Ответ: " + calculator.result);
     }
 }
